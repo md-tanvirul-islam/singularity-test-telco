da87b175625eca483fdd68ae25faf515dd7ea296
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-08-25 04:07:55 +0600

    outlet model fillable

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Outlet extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'phone', 'latitude', 'longitude', 'image'
+    ];
 }
