4d72e87c3baa76b37ce8cfc6de4512d6a547b768
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-08-25 03:53:58 +0600

    navbar - user list

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,9 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                        {{ __('Users') }}
-                    </x-nav-link>
+                    @if (Auth::user()->can('viewAny', App\Models\User::class))
+                        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                            {{ __('Users') }}
+                        </x-nav-link>
+                    @endif
                     <x-nav-link :href="route('outlets.index')" :active="request()->routeIs('outlets.index')">
                         {{ __('Outlets') }}
                     </x-nav-link>
